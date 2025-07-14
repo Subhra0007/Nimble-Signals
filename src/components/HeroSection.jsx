2834de2212f345895bd6a3108d49dc74c958c768
Subhra007 <berasubhra2004@gmail.com>
2025-07-14 20:50:48 +0530

    Update Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,27 +2,32 @@ import hero1 from '../assets/hero1.gif';
 import hero2 from '../assets/hero2.gif';
 import hero3 from '../assets/hero3.gif';
 import hero5 from '../assets/hero5.gif';
-function HeroSection (){
-      return(
-            <>
-            <div id='#home' className='w-full flex flex-row items-center justify-between '>
-                  <div className='flex w-1/2 flex-col mt-30'>
-                        <h1 className='text-9xl font-bold uppercase'>Here to connect</h1>
-                        <h4 className='text-4xl uppercase font-medium mt-10 text-fuchsia-600'>Untangle the web with us</h4>
-                        <div>
-                              <button className='mt-40 bg-gradient-to-r from-[#4949a6] to-[#c757be] text-white py-4 px-10 rounded-4xl text-2xl font-medium uppercase'>Connect here</button>
-                        </div>
-                        
-                  </div>
-                  <div className='flex w-1/2 flex-wrap flex-row pl-40'>
-                        <img src={hero1} alt='hero1' className='w-60 h-80'/>
-                        <img src={hero2} alt='hero2' className='w-50 h-70'/>
-                        <img src={hero3} alt='hero3' className='w-60 h-80  mt-0'/>
-                        <img src={hero5} alt='hero5' className='w-50 h-80  mt-0'/>
-                  </div>
-             </div>
-            </>
-      )
+
+function HeroSection() {
+  return (
+    <div id="home" className="min-h-screen grid grid-cols-1 md:grid-cols-2 items-center w-full  pl-10  pr-10 ">
+      
+      <div className="w-full">
+        <h1 className="text-8xl  font-bold uppercase">
+          Here to <br /> connect
+        </h1>
+        <h4 className="text-2xl md:text-4xl uppercase font-medium mt-10 text-fuchsia-400">
+          Untangle the web with us
+        </h4>
+        <button className="mt-20 bg-gradient-to-r from-[#4949a6] to-[#c757be] py-4 px-10 rounded-4xl text-2xl  font-medium uppercase">
+          Connect here
+        </button>
+      </div>
+
+      {/* Right Section */}
+      <div className="grid grid-cols-2 gap-6 place-items-center w-full mt-10 ">
+        <img src={hero1} alt="hero1" className="w-60 h-80" />
+        <img src={hero2} alt="hero2" className="w-60 h-80" />
+        <img src={hero3} alt="hero3" className="w-60 h-80" />
+        <img src={hero5} alt="hero5" className="w-60 h-80" />
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection;
+export default HeroSection;
